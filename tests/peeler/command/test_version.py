1b5a95e5b2575e8bca9a9d4ede79938c052c1bec
Maxime Letellier <drumsticks@live.fr>
2025-01-02 10:58:33 +0100

    Add a test for version command

---
+++ tests/peeler/command/test_version.py
@@ -0,0 +1,17 @@
+import re
+
+from pytest import CaptureFixture
+
+from peeler.command.version import version_command
+
+
+package_name_regex = f"peeler"
+# from https://semver.org/spec/v2.0.0.html
+version_regex = r"(0|[1-9]\d*)\.(0|[1-9]\d*)\.(0|[1-9]\d*)(?:-((?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*)(?:\.(?:0|[1-9]\d*|\d*[a-zA-Z-][0-9a-zA-Z-]*))*))?(?:\+([0-9a-zA-Z-]+(?:\.[0-9a-zA-Z-]+)*))?"
+
+
+def test_version_command(capfd: CaptureFixture) -> None:
+    version_command()
+    out, err = capfd.readouterr()
+
+    assert re.compile(f"^{package_name_regex} {version_regex}$").match(out)
