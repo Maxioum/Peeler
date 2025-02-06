84945d9ddb6bd3255ea0ef73ff77c18c566f202f
Maxioum <maxime.eliot.letellier@gmail.com>
2025-02-06 20:26:05 +0100

    Apply pre-commit hook insert license

--- peeler/pyproject/validator.py
+++ peeler/pyproject/validator.py
@@ -1,3 +1,7 @@
+# # SPDX-FileCopyrightText: 2025 Maxime Letellier <maxime.eliot.letellier@gmail.com>
+#
+# # SPDX-License-Identifier: GPL-2.0-or-later
+
 from typing import Any, Dict
 from pathlib import Path
 
