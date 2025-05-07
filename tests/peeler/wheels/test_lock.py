f4f938c1caa8701fddf50c18b8483b580899b9d5
Maxime Letellier <9801590+Maxioum@users.noreply.github.com>
2025-05-07 13:26:05 +0200

    Add tests for pylock files

--- tests/peeler/wheels/test_lock.py
+++ tests/peeler/wheels/test_lock.py
@@ -1,8 +1,10 @@
 from pathlib import Path
+from typing import Type
 
+from pytest import mark
 from tomlkit import TOMLDocument
 
-from peeler.wheels.lock import _get_lock_file, _get_wheels_urls_from_lock
+from peeler.wheels.lock import AbstractURLFetcherStrategy, PylockUrlFetcher, PyprojectUVLockFetcher, UVLockUrlFetcher, UrlFetcherCreator, _generate_uv_lock, _get_wheels_urls_from_lock
 
 
 def test__get_wheels_urls_from_lock(lock_file: TOMLDocument) -> None:
@@ -13,14 +15,30 @@ def test__get_wheels_urls_from_lock(lock_file: TOMLDocument) -> None:
 
 
 def test__get_lock_file(pyproject_path_with_lock: Path) -> None:
-    with _get_lock_file(pyproject_path_with_lock) as lock_file:
+    with _generate_uv_lock(pyproject_path_with_lock) as lock_file:
         assert lock_file.exists()
 
     assert lock_file.exists()
 
 
 def test__get_lock_file_no_lock_file(pyproject_path_without_lock: Path) -> None:
-    with _get_lock_file(pyproject_path_without_lock) as lock_file:
+    with _generate_uv_lock(pyproject_path_without_lock) as lock_file:
         assert lock_file.exists()
 
     assert not lock_file.exists()
+
+_DATA = Path(__file__).parent / "data"
+
+@mark.parametrize(
+    ("url_fetcher_creator", "expected_strategy"),
+    [
+        (_DATA / "pyproject.toml", PyprojectUVLockFetcher),
+        (_DATA / "uv.lock", UVLockUrlFetcher),
+        (_DATA / "pylock.toml", PylockUrlFetcher),
+        (_DATA / "pylock.test.toml", PylockUrlFetcher),
+    ],
+    indirect=["url_fetcher_creator"]
+)
+def test_url_fetcher_creator_get_fetch_url_strategy(url_fetcher_creator: UrlFetcherCreator, expected_strategy: Type[AbstractURLFetcherStrategy]) -> None:
+
+    assert isinstance(url_fetcher_creator.get_fetch_url_strategy(), expected_strategy)
